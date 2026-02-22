590c031d63a609343c091746397696b919a24c24
Twaran Gupta <twarangupta01@gmail.com>
2026-02-23 02:23:36 +0530

    added launch json to start backend server

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'database.sqlite');
+const dbPath = path.resolve(__dirname, 'database2.sqlite');
 
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
